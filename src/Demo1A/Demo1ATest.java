e8c49bb96508cf02a6ddd622a5e11a348dd2ad23
sameerh7944 <sayedsameer250@gmail.com>
2025-01-30 19:46:30 -0500

    Update Demo1ATest.java
    new variable added

--- src/Demo1A/Demo1ATest.java
+++ src/Demo1A/Demo1ATest.java
@@ -4,7 +4,10 @@ public class Demo1ATest {
 	public static void main(String[]args) {
 		
 		System.out.println("United States Of America");
-	
+	int num = 12;
+		if (num > 5) {
+			System.out.println("Number is Greater than 5")
+				}
 	        int a = 10; 
 	        System.out.println("print 10"); 
 	    }
