8cdc88972c12887ba7bb09bc396fcbc4eac5595d
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-30 19:32:03 -0500

    variable added

--- src/Demo1A/Demo1ATest.java
+++ src/Demo1A/Demo1ATest.java
@@ -4,6 +4,9 @@ public class Demo1ATest {
 	public static void main(String[]args) {
 		
 		System.out.println("United States Of America");
+	
+	        int a = 10; 
+	        System.out.println("print 10"); 
+	    }
 	}
 
-}
